445c6bd4a3a10ac993a5e4af6f51773fa2392c66
Katze Studios <132407218+katzestudios@users.noreply.github.com>
2024-07-07 14:36:37 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,8 +1,11 @@
 RegisterServerEvent('inventory:getInventory')
-AddEventHandler('inventory:getInventory', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
+AddEventHandler('inventory:getInventory', function(playerId)
+    local xPlayer = ESX.GetPlayerFromId(playerId)
     if xPlayer then
+
         local inventory = xPlayer.getInventory()
         TriggerClientEvent('inventory:showInventory', source, inventory)
+        local data = {title = 'Achtung!', description = 'Du wirst durchsucht!', type = 'warning'}
+        TriggerClientEvent('ox_lib:notify', playerId, data)
     end
 end)
