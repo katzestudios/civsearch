0ce340ca27d9838953d0bbcea3a8bf6264bdcd54
Katze Studios <132407218+katzestudios@users.noreply.github.com>
2024-07-07 14:36:51 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,17 @@
 fx_version 'cerulean'
-shared_script '@es_extended/imports.lua'
+game 'gta5'
+shared_scripts {
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua',
+    '@ox_target'
+}
+
+
 server_script 'server.lua'
-client_script 'client.lua'
+client_scripts {
+    
+    'client.lua',
+    '@NativeUI/NativeUI.lua'
+}
+
+lua54 'yes'
